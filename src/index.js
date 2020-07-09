b070d0b4a9bff85fad6102ff0455c09bece79c83
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-07-09 13:14:36 +0200

    update project

--- src/index.js
+++ src/index.js
@@ -1,89 +1,57 @@
-// npm imports at the top of the file!
+// Imports at the top of the file!
+// We never nest imports inside blocks of code!
 import axios from 'axios'
+import { breeds } from './breeds'
 
-const breeds = [
-  'mastiff',
-  'affenpinscher',
-  'australian',
-  'mexicanhairless',
-  'cocker',
-]
 
-// Declaration of a function that returns a promise.
-// It's not common that we would need to do this.
-function onlyLikeEvenNumbers(number) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (number % 2 === 0) {
-        resolve(`Good: ${number} is even`)
-      } else {
-        reject(`Bad: ${number} is odd`)
-      }
-    }, 1000)
-  })
-}
+// 👉 TASK 1- Test out the endpoint `https://dog.ceo/api/breeds/image/random`
+//    * With HTTPie (command-line tool)
+//    * With Postman (tool with GUI)
+//    * With Chrome
 
-// Usage of a function that returns a promise.
-// Very common to use utility functions that return promises:
-// axios.get('URL') // returns a promise
-onlyLikeEvenNumbers(5)
-  .then(success => {
-    console.log(success)
-  })
-  .catch(error => {
-    console.log(error)
-  })
-  .finally(() => {
-    console.log('regardless')
-  })
 
-// 👉 TASK 1- Select the "entry point", the element
+// 👉 TASK 2- Select the "entry point", the element
 // inside of which we'll inject our dog cards 
 const entryPoint = document.querySelector('.entry')
 
 
-// 👉 TASK 2- Complete the `dogCardMaker` component,
-// which takes an object { imageURL, breed } as its argument
-// and creates the following elements:
-
-// <div class="dog-card">
-//   <img src={imageURL} class="dog-image">
-//   <h3>Breed: {breed}</h3>
-// </div>
+// 👉 TASK 3- `dogCardMaker` takes an object and returns a Dog Card.
+// Use this function to build a Card, and append it to the entry point.
 function dogCardMaker({ imageURL, breed }) {
+  // instantiating the elements
   const dogCard = document.createElement('div')
   const image = document.createElement('img')
   const heading = document.createElement('h3')
-
+  // setting class names, attributes and text
   heading.textContent = `Breed: ${breed}`
   image.src = imageURL
   image.classList.add('dog-image')
   dogCard.classList.add('dog-card')
+  // creating the hierarchy
   dogCard.appendChild(image)
   dogCard.appendChild(heading)
-
-  // add this functionality later
+  // adding some interactivity
   dogCard.addEventListener('click', () => {
     dogCard.classList.toggle('selected')
   })
-
+  // never forget to return!
   return dogCard
 }
 
 
-// 👉 TASK 3- Bring the Axios library into the project using one of two methods:
+// 👉 TASK 4- Bring the Axios library into the project using one of two methods:
 //    * Traditional way: put another script tag inside index.html (`https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js`)
 //    * Proyects with npm: install it with npm and import it into this file
 
 
-// 👉 TASK 4- Fetch dogs from `https://dog.ceo/api/breed/{breed}/images/random/{number}`
+// 👉 TASK 5- Fetch dogs from `https://dog.ceo/api/breed/{breed}/images/random/{number}`
 //    * ON SUCCESS: use the data to create dogCards and append them to the entry point
 //    * ON FAILURE: log the error to the console
 //    * IN ANY CASE: log "done" to the console
 //    (instructor will demo fetching a random dog from `https://dog.ceo/api/breeds/image/random`)
 
 
-// 👉 TASK 5- Wrap the fetching operation inside a function `getDogs`
+// 👉 TASK 6- Wrap the fetching operation inside a function `getDogs`
 // that takes a breed and a count (of dogs)
 function getDogs(breed, count) {
   axios.get(`https://dog.ceo/api/breed/${breed}/images/random/${count}`)
@@ -102,7 +70,7 @@ function getDogs(breed, count) {
 }
 
 
-// 👉 TASK 6- Put a button in index.html to 'get dogs' and add a click
+// 👉 TASK 7- Put a button in index.html to 'get dogs' and add a click
 // event listener that executes `getDogs`
 document.querySelector('button').addEventListener('click', evt => {
   getDogs('mastiff', 3)
@@ -110,4 +78,4 @@ document.querySelector('button').addEventListener('click', evt => {
 })
 
 
-// 👉 TASK 7- Loop over the breeds array, fetching a dog at each iteration
+// 👉 TASK 8- Loop over the breeds array, fetching a dog at each iteration
